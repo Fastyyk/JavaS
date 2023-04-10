985e66fc3070bfbf6ec8932ec18d3d9a74381d63
Fastyyk <116104906+Fastyyk@users.noreply.github.com>
2023-04-10 21:01:25 +0300

    87654356756

---
+++ script (1).js
@@ -0,0 +1,14 @@
+
+
+let voys1 - prompt("Тобі 66?")
+if  voys1 ('так')
+    alert ('зроз')
+    else
+    let (voys2= prompt (Україна Переможе орків ?))
+    if voys2  ('Так')
+      alert ('')
+         else 
+            let voys3 = prompt('Слава Україні')
+                if (voys3 -- 'Героям Слава')
+                  alert (Скажеш Паляниця ?)
+ 
